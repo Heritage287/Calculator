ed10dcfc324037cf2e948002103d46e12b6a5901
Heritage Abraham <thatsheritage@email.com>
2021-11-10 10:20:49 +0100

    add new changes

---
+++ README.md
@@ -0,0 +1,11 @@
+# CALCULATOR
+technologies used
+* Html
+* Js
+
+[Click here](index.html)
+# to view calculator
+[Click here](js/script.js)
+# to view js code
+
+
---
+++ js/script.js
@@ -0,0 +1,17 @@
+let num=parseInt(prompt("Enter your first number"));
+let number=parseInt(prompt("Enter your second number"));
+let a=num+number
+let b=num-number
+let c=num*number
+let d=num/number
+
+
+function Num(arithimetic){
+
+alert(arithimetic);
+
+}
+Num("The addition is "+a)
+Num("The subtraction is "+b)
+Num("The multiplication is "+c)
+Num("The division is "+d)
